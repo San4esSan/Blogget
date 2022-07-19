2ad834b5d7068b5a21f0d0f77bf225511a891fb2
mister.San4es <mister.San4es@list.ru>
2022-07-19 12:45:56 +0400

    fix tab end style date

--- src/components/Main/Tabs/Tabs.jsx
+++ src/components/Main/Tabs/Tabs.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import style from './Tabs.module.css';
 
-export const Tabs = () => {
-  return (
-    <ul className={style.list}>
-      <li>
-        <a href="/">Главная</a>
-      </li>
-      <li>
-        <a href="/">Просмотренные</a>
-      </li>
-      <li>
-        <a href="/">Сохраненные</a>
-      </li>
-      <li>
-        <a href="/">Мои посты</a>
-      </li>
-    </ul>
-  );
-};
+export const Tabs = () => (
+  <ul className={style.list}>
+    <li>
+      <a href="/">Главная</a>
+    </li>
+    <li>
+      <a href="/">Просмотренные</a>
+    </li>
+    <li>
+      <a href="/">Сохраненные</a>
+    </li>
+    <li>
+      <a href="/">Мои посты</a>
+    </li>
+  </ul>
+);
